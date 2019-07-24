b0fddb08a33d7fd74a0a68162fd82f12cff0c423
exgamer@live.ru <1037213109sex>
2019-07-24 20:49:45 +0600

    f

---
+++ src/helpers/TreeHelper.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace concepture\core\helpers;
+
+class TreeHelper
+{
+
+    public function buildTree(array &$elements, $parentId = 0, $from = 'id', $to = "title", $parent = 'parent_id')
+    {
+        $branch = array();
+        foreach ($elements as $element) {
+            if ($element[$parent] == $parentId) {
+                $children = static::buildTree($elements, $element[$from]);
+                if ($children) {
+                    $branch[$element[$to]] = $children;
+                }else
+                    $branch[$element[$from]] = $element[$to];
+                unset($elements[$element[$from]]);
+            }
+        }
+
+        return $branch;
+    }
+}
+
