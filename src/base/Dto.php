32202d83b5046ca37b45adc71f40243e00c644cc
exgamer@live.ru <1037213109sex>
2019-08-31 21:11:06 +0600

    f

--- src/base/Dto.php
+++ src/base/Dto.php
@@ -126,4 +126,24 @@ abstract class Dto extends BaseObject
 
         return $result;
     }
+
+    public function __set($name, $value)
+    {
+        $setter = 'set' . $name;
+        if (method_exists($this, $setter)) {
+            $this->$setter($value);
+        } elseif (method_exists($this, 'get' . $name)) {
+            throw new \Exception('Setting read-only property: ' . get_class($this) . '::' . $name);
+        }
+    }
+
+    public function __get($name)
+    {
+        $getter = 'get' . $name;
+        if (method_exists($this, $getter)) {
+            return $this->$getter();
+        } elseif (method_exists($this, 'set' . $name)) {
+            throw new \Exception('Getting write-only property: ' . get_class($this) . '::' . $name);
+        }
+    }
 }
